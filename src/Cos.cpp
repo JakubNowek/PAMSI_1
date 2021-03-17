3ed1de8e4bf8674264546fadde281afa49a4fe50
JakubNowek <jakubbnowek@gmail.com>
2021-03-17 09:06:27 +0100

    Próba#1.2 - Zobaczmymy czy zadziała cmake

---
+++ src/Cos.cpp
@@ -0,0 +1,9 @@
+#include "cos.hh"
+#include <iostream>
+
+using namespace std;
+
+void Cos(){
+    cout<<"Jesli to widzisz to dziala funkcja Cos"<<endl;
+}
+
--- src/Main.cpp
+++ src/Main.cpp
@@ -1,7 +1,9 @@
 #include <iostream>
+#include "cos.hh"
 
 int main(){
     std::cout<<"nowe repozytorium"<<std::endl;
     //test gita
+    Cos();
     return 0;
 }
